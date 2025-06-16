6348ca598a19e4b1d7e67f2906942d8fc7b27b9a
hennotheo <theo.henno21@gmail.com>
2025-06-16 22:08:44 +0200

    :tada: Init Project

---
+++ src/client/java/com/theohenno/narcmod/NarcModClient.java
@@ -0,0 +1,10 @@
+package com.theohenno.narcmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NarcModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
