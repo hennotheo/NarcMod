819ac599eefe0293ba805885063ee9053a96beef
hennotheo <theo.henno21@gmail.com>
2025-06-17 23:48:33 +0200

    :sparkles: Add Basic test Block and Item

--- src/main/java/com/theohenno/narcmod/NarcMod.java
+++ src/main/java/com/theohenno/narcmod/NarcMod.java
@@ -2,23 +2,27 @@ package com.theohenno.narcmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Block;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NarcMod implements ModInitializer {
-	public static final String MOD_ID = "narcmod";
+    public static final String MOD_ID = "narcmod";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        com.theohenno.narcmod.items.ModItems.initialize();
+        com.theohenno.narcmod.blocks.ModBlocks.initialize();
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        LOGGER.info("Hello Fabric world!");
+    }
 }
---
+++ src/main/java/com/theohenno/narcmod/items/ModItems.java
@@ -0,0 +1,36 @@
+package com.theohenno.narcmod.items;
+
+import com.theohenno.narcmod.NarcMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+    }
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NarcMod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+}
